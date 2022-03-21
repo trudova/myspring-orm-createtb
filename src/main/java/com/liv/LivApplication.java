e806b108d26c4f255101c243fc8f9814919c75d8
trudova <liubovtrudova@gmail.com>
2022-03-21 09:30:34 -0400

    first commit

---
+++ src/main/java/com/liv/LivApplication.java
@@ -0,0 +1,13 @@
+package com.liv;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LivApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LivApplication.class, args);
+    }
+
+}
